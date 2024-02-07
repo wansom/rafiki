bfb274e59f292507b4d57d203a56e30c863aa993
wansom <ochiengwarren10@gmail.com>
2024-02-07 16:41:16 +0300

    change message

--- src/components/PaymentsForm.tsx
+++ src/components/PaymentsForm.tsx
@@ -129,7 +129,7 @@ const PaymentsForm = () => {
                 href="#"
                 className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
                 onClick={() => {
-                  handleClick(100)
+                  handleClick(1500)
                 }}
               >
                 Get started
